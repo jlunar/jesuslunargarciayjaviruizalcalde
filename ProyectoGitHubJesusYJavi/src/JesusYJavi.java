6d1c0e588a1746928ce8a2bc52a633b4a061f392
Jesus <Jesus@Jesus-PC>
2013-05-14 14:37:44 +0200

    commit para subirlo al servidor

--- ProyectoGitHubJesusYJavi/src/JesusYJavi.java
+++ ProyectoGitHubJesusYJavi/src/JesusYJavi.java
@@ -7,6 +7,7 @@ public class JesusYJavi {
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
 		System.out.println("hola Jaime estamos haciendo el gitHub");
+		System.out.println("hola");
 	}
 
 }
